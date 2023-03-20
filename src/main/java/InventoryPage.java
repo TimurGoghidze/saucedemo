d660abb47e5b0844c5ecc86342d09ea8862723c3
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-20 20:55:24 +0100

    add  tests for InventoryPageTests

--- src/main/java/InventoryPage.java
+++ src/main/java/InventoryPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.*;
 
 public class InventoryPage extends BasePage {
@@ -14,10 +15,35 @@ public class InventoryPage extends BasePage {
     private WebElement productsTitle;
 
     @FindBy(id = "react-burger-menu-btn")
-    private WebElement inventoryList;
+    private WebElement sideBarButton;
 
     @FindBy(css = "[class='inventory_item']")
     private List<WebElement> productCards; // коллекция карточек товара
+
+    @FindBy(css = "[class='inventory_item_name']")
+    private List<WebElement> productCardsName;
+
+    @FindBy(xpath = "//div[@class='inventory_item_img']")
+    private List<WebElement> productCardsPicture;
+
+    @FindBy(css = "[class='btn btn_primary btn_small btn_inventory']")
+    private List<WebElement> buttonAddToCart;
+
+    @FindBy(xpath = "//a[@href='https://twitter.com/saucelabs']")
+    private WebElement twitterIconLink;
+
+    @FindBy(id = "inventory_sidebar_link")
+    private WebElement allItems;
+
+    @FindBy(id = "about_sidebar_link")
+    private WebElement about;
+
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logOut;
+
+    @FindBy(id = "reset_sidebar_link")
+    private WebElement resetAppState;
+
 
     public InventoryPage(WebDriver driver) {
         super(driver);
@@ -27,7 +53,7 @@ public class InventoryPage extends BasePage {
     }
 
     public void inventoryListShouldBeDisplayed() {
-        assertTrue("Element " + "inventoryList" + "is NOT visible", inventoryList.isDisplayed());
+        assertTrue("Element " + "inventoryList" + "is NOT visible", sideBarButton.isDisplayed());
     }
 
     public void checkAllProductsAreDisplayed() { //проверка списка товаров
@@ -41,4 +67,57 @@ public class InventoryPage extends BasePage {
         assertEquals("Amount of products is not " + expectedAmount, productCards.size(), expectedAmount);
     }
 
+    public void checkProductNameIsDisplayed(){
+        for (WebElement productname :
+                productCardsName) {
+            assertTrue(productname.isDisplayed());
+        }
+    }
+
+    public void checkProductNameIsNotEmpty() {
+        for (WebElement productname:
+             productCardsName) {
+            assertTrue(productname.isDisplayed());
+        }
+
+    }
+
+    public void checkProductCardPictureIsDisplayed(){
+        for (WebElement picture :
+                productCardsPicture) {
+            assertTrue(picture.isDisplayed());
+        }
+    }
+    public void checkProductCardPictureIsNotEmpty(int expectedAmount) { //метод подсчёта количества отражаемых карточек товара
+        assertEquals("Amount of products is not " + expectedAmount, productCardsPicture.size(), expectedAmount);
+    }
+    public void checkButtonAddToCartIsDisplayed(){
+        for (WebElement addtocart :
+                buttonAddToCart) {
+            assertTrue(addtocart.isDisplayed());
+        }
+    }
+    public void checkButtonAddToCartIsNotEmpty(int expectedAmount) { //метод подсчёта количества отражаемых карточек товара
+        assertEquals("Amount of products is not " + expectedAmount, buttonAddToCart.size(), expectedAmount);
+    }
+    public void checkTwitterLink(){
+        assertTrue(twitterIconLink.isDisplayed());
+    }
+    public void clickOnSideBarOpenIcon() throws InterruptedException {
+        sideBarButton.click();
+        sleep(1000);
+    }
+
+    public void allItemsIsDisplayed(){
+        assertTrue(allItems.isDisplayed());
+    }
+    public void aboutIsDisplayed(){
+        assertTrue(about.isDisplayed());
+    }
+    public void logoutIsDisplayed(){
+        assertTrue(logOut.isDisplayed());
+    }
+    public void resetAppStateIsDisplayed(){
+        assertTrue(resetAppState.isDisplayed());
+    }
 }
--- src/test/java/InventoryPageTests.java
+++ src/test/java/InventoryPageTests.java
@@ -10,6 +10,32 @@ public class InventoryPageTests extends TestBase {
         InventoryPage inventoryPage = new InventoryPage(driver);
         inventoryPage.checkAllProductsAreDisplayed(); // на наличие карточек
         inventoryPage.checkAmountOfProducts(6); // сверяемся с заявленными количеством
+        inventoryPage.checkProductNameIsDisplayed(); // наличие названий
+        inventoryPage.checkProductNameIsNotEmpty(); //названия не пусты
+        inventoryPage.checkProductCardPictureIsDisplayed(); //наличие картинок
+        inventoryPage.checkProductCardPictureIsNotEmpty(6);
+        inventoryPage.checkButtonAddToCartIsDisplayed(); //наличие кнопок добавить в корзину
+        inventoryPage.checkButtonAddToCartIsNotEmpty(6); //их количество
     }
 
+    @Test
+    public void allElementsAreDisplayed() {
+        User user = new User(validUsername, validPassword);
+        new LoginPage(driver).login(user).inventoryListShouldBeDisplayed();
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.checkTwitterLink();
+
+    }
+
+    @Test
+    public void sideBar() throws InterruptedException {
+        User user = new User(validUsername, validPassword);
+        new LoginPage(driver).login(user).inventoryListShouldBeDisplayed();
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.clickOnSideBarOpenIcon();
+        inventoryPage.allItemsIsDisplayed();
+        inventoryPage.aboutIsDisplayed();
+        inventoryPage.logoutIsDisplayed();
+        inventoryPage.resetAppStateIsDisplayed();
+    }
 }
